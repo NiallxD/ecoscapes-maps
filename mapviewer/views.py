3d8c7e939de0aa1002ac069307041f6416bb4350
Niall Bell <niallbell@hotmail.co.uk>
2025-10-28 13:50:04 -0700

    Initial gh-pages setup

---
+++ mapviewer/views.py
@@ -0,0 +1,28 @@
+import json
+import os
+from django.conf import settings
+from django.http import JsonResponse, Http404
+from django.shortcuts import render
+
+def load_map_config():
+    config_path = os.path.join(settings.BASE_DIR, 'map_config.json')
+    try:
+        with open(config_path, 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return {}
+
+def map_view(request, page_name):
+    config = load_map_config()
+    
+    if page_name not in config:
+        raise Http404("Page not found")
+    
+    page_config = config[page_name]
+    context = {
+        'map1_url': page_config.get('map1_url', ''),
+        'map2_url': page_config.get('map2_url', ''),
+        'page_name': page_name,
+    }
+    
+    return render(request, 'mapviewer/map.html', context)
