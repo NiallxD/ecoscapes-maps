8b0bd81f20dc832d16eaf438617cda43f3517c41
Niall Bell <niallbell@hotmail.co.uk>
2025-12-05 16:44:31 -0800

    Updated page structure and tweaked import script

--- fantasticon-config.js
+++ fantasticon-config.js
@@ -6,7 +6,9 @@ module.exports = {
   assetTypes: ['css'],
   prefix: 'icon',
   selector: '.icon',
-  fontsUrl: '/static/fonts',
+  // Use the full URL for fontsUrl to ensure it's used in the generated CSS
+  fontsUrl: 'https://niallxd.github.io/ecoscapes-maps/static/fonts',
+  // Use a simpler pathOptions format
   pathOptions: {
     css: './static/fonts/custom-icons.css'
   }
